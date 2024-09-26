024f5b018ea82bd63c4ca9112ba1141b01d8d921
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-26 22:50:55 +0900

    Update start.py

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 st.header("✨인공지능 영어 선생님 잉글링👩‍🏫")
+st.devider()
 st.write("1️⃣ 대화 훈련: ✨인공지능으로 이번 단원 주요표현 훈련하기")
 st.write("2️⃣ 듣기 퀴즈: ✨인공지능으로 영어 듣기 문제 풀기")
 st.write("3️⃣ 단어 퀴즈: ✨인공지능으로 이번 단원 영어 단어 퀴즈 풀기")

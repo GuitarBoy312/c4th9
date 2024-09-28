576f3bf67f5add207b3402e83eea51af118952a9
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-29 02:38:35 +0900

    Update 5 5.013 이미지 크리에이트.py

--- pages/5 5.013 이미지 크리에이트.py
+++ pages/5 5.013 이미지 크리에이트.py
@@ -95,7 +95,7 @@ if st.button("새 문제 만들기"):
 
 if st.session_state.question_generated:
     st.markdown("### 문제")
-    st.write(f"빈칸을 채워 전체 단어를 입력하세요: {st.session_state.blanked_word} {st.session_state.emoji}")
+    st.write(f"빈칸을 채워 전체 단어를 입력하세요. 이모티콘과 그림을 참고하세요.: {st.session_state.blanked_word} {st.session_state.emoji}")
     
     if st.session_state.image_url:
         st.image(st.session_state.image_url, caption="단어 관련 이미지", width=256)  # 너비를 256으로 설정

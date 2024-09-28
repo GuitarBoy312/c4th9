7444aac5c538a47dfb91e62871915927d0878dde
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-29 02:02:01 +0900

    Update 5 5.013 이미지 크리에이트.py

--- pages/5 5.013 이미지 크리에이트.py
+++ pages/5 5.013 이미지 크리에이트.py
@@ -19,7 +19,7 @@ def generate_image(word):
     try:
         response = client.images.generate(
             model="dall-e-2",
-            prompt=f"cute 3d art of {word} that ordinary elementary school studend can guess {word}",
+            prompt=f"japanese animation style illustration of {word} that ordinary elementary school studend can guess {word}",
             size="256x256",
             n=1,
         )

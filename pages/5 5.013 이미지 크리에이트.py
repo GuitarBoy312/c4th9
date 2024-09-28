98da1a216b7f8847678f753d943678fa7086f4a1
GuitarBoy312 <169133828+GuitarBoy312@users.noreply.github.com>
2024-09-29 01:58:28 +0900

    Update 5 5.013 이미지 크리에이트.py

--- pages/5 5.013 이미지 크리에이트.py
+++ pages/5 5.013 이미지 크리에이트.py
@@ -19,7 +19,7 @@ def generate_image(word):
     try:
         response = client.images.generate(
             model="dall-e-2",
-            prompt=f"simple drawing of {word} that student can guess {word}"
+            prompt=f"simple drawing of {word} that student can guess {word}",
             size="256x256",
             n=1,
         )
